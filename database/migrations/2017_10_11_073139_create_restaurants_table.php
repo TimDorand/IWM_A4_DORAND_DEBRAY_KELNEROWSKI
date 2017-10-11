23fec29164174523dc47a3c6055d9ed3af5f82f3
Florian KELNEROWSKI <florian@groupesenef.com>
2017-10-11 11:02:00 +0200

    migrations + appserviceprovider

---
+++ database/migrations/2017_10_11_073139_create_restaurants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('lat');
+            $table->float('long');
+            $table->integer('g_id');
+            $table->string('name');
+            $table->longText('icon');
+            $table->longText('infos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('restaurants');
+    }
+}
