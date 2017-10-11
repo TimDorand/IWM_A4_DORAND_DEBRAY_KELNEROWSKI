7d3947090e2e690a1a9fd34494f60e676cfa8b07
timothee <tim.dorand@gmail.com>
2017-10-11 12:03:50 +0200

    init styling ui

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,69 +11,100 @@
     <title>{{ config('app.name', 'Eat Nearby') }}</title>
 
     <!-- Styles -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.css" />
-    {{--<link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
+    {{--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.0/css/bulma.css"/>--}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.5.3/css/bulma.min.css" integrity="sha256-spCEAaZMKebC3rE/ZTt8jITn65b0Zan45WXblWjyDyQ=" crossorigin="anonymous" />
+
+{{--    <link href="{{ asset('css/app.css') }}" rel="stylesheet">--}}
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
+        <div class="container" id="app">
+            <nav class="nav has-shadow">
+                <div class="container">
+                    <div class="nav-left">
+                        <a class="nav-item" href="{{url('/')}}">
+                                {{ config('app.name', 'Eat Nearby') }}
+                        </a>
+                    </div>
+                    <span class="nav-toggle">
+                      <span></span>
+                      <span></span>
+                      <span></span>
+                    </span>
+                    <div class="nav-right nav-menu">
+                       {{-- <a class="nav-item is-tab is-active">
+                            Paris
+                        </a>
+                        <a class="nav-item is-tab is-active">
+                            New York
+                        </a>
+                        <a class="nav-item is-tab">
+                            Help
+                        </a>--}}
+                        <span class="nav-item">
+                            <div class="field is-grouped">
+                                @guest
+                                <p class="control">
+                                  <a class="button " href="{{ route('login') }}">Login</a>
+                                <a class="button" href="{{ route('register') }}">Register</a>
+                              </p>
+                                @else
+                                    <div class="dropdown is-active">
+                                      <div class="dropdown-trigger">
+                                        <button class="button" aria-haspopup="true" aria-controls="dropdown-menu">
+                                          <span>{{ Auth::user()->name }}</span> <span class="caret"></span>
+                                          <span class="icon is-small">
+                                            <i class="fa fa-angle-down" aria-hidden="true"></i>
+                                          </span>
+                                        </button>
+                                      </div>
+                                      <div class="dropdown-menu" id="dropdown-menu" role="menu">
+                                        <div class="dropdown-content">
+                                         <a href="{{ route('logout') }}" class="dropdown-item"
+                                                 onclick="event.preventDefault();
+                                                                 document.getElementById('logout-form').submit();">
+                                                        Logout
+                                                    </a>
+                                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                                        {{ csrf_field() }}
+                                                    </form>
+                                          <a class="dropdown-item">
+                                            Other dropdown item
+                                          </a>
+                                          <a href="#" class="dropdown-item is-active">
+                                            Active dropdown item
+                                          </a>
+                                          <a href="#" class="dropdown-item">
+                                            Other dropdown item
+                                          </a>
+                                          <hr class="dropdown-divider">
+                                          <a href="#" class="dropdown-item">
+                                            With a divider
+                                          </a>
+                                        </div>
+                                      </div>
+                                    </div>
+                                    @endguest
+                                </div>
+                            </span>
+                    </div>
                 </div>
-
-                <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li>
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-        @yield('content')
+            </nav>
     </div>
+
+
+
+    @yield('content')
+
+    <div class="hero-foot">
+        <div class="container">
+            <div class="tabs is-centered">
+                <ul>
+                    <li><a>And this at the bottom</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+{{--<script async type="text/javascript" src="../js/bulma.js"></script>--}}
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,93 +1,120 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <style>
-            /* Always set the map height explicitly to define the size of the div
-             * element that contains the map. */
-            #map {
-                height: 1000px !important;
-                width: 1000px !important;
-            }
-            /* Optional: Makes the sample page fill the window. */
-            html, body {
-                height: 100%;
-                margin: 0;
-                padding: 0;
-            }
-        </style>
 
+    <section class="hero has-text-centered is-small is-success is-bold">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title">
+                    Eat cheap, fast, healthy or all 3!
+                </h1>
+                {{--<h2 class="subtitle">
+                    Find nearby places and eat what you want !
+                </h2>--}}
+            </div>
+        </div>
+    </section>
+    <div class="container-fluid">
 
-        <script>
+        <div class="columns is-vcentered">
+            <div class="column">
+                <div class="container">
 
-            var map;
-            var infowindow;
+                <h1 class="title is-2">
+                    Superhero Scaffolding
+                </h1>
+                <h2 class="subtitle is-4">
+                    Let this cover page describe a product or service.
+                </h2>
+                <br>
+                <p class="has-text-centered">
+                    <a class="button is-large">
+                        Learn more
+                    </a>
+                </p>
+                {{--
+                <figure class="image is-4by3">
+                    <img src="http://placehold.it/800x600" alt="Description">
+                </figure>--}}
+                </div>
+            </div>
+            <div class="column">
+                <div id="map"></div>
 
-            function initMap(latitude, longitude) {
-                var pyrmont = {lat: latitude, lng: longitude};
-
-                map = new google.maps.Map(document.getElementById('map'), {
-                    center: pyrmont,
-                    zoom: 15
-                });
-
-                infowindow = new google.maps.InfoWindow();
-                var service = new google.maps.places.PlacesService(map);
-                service.nearbySearch({
-                    location: pyrmont,
-                    radius: 500,
-                    type: ['store']
-                }, callback);
-            }
-
-            function callback(results, status) {
-                if (status === google.maps.places.PlacesServiceStatus.OK) {
-                    for (var i = 0; i < results.length; i++) {
-                        createMarker(results[i]);
-                    }
-                }
-            }
-
-            function createMarker(place) {
-                var placeLoc = place.geometry.location;
-                var marker = new google.maps.Marker({
-                    map: map,
-                    position: place.geometry.location
-                });
-
-                google.maps.event.addListener(marker, 'click', function() {
-                    infowindow.setContent(place.name);
-                    infowindow.open(map, this);
-                });
-            }
-        </script>
-
-        <div id="map"></div>
-
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-        <script>
-            function getLocation() {
-                if (navigator.geolocation) {
-                    navigator.geolocation.getCurrentPosition(savePosition, positionError, {timeout:10000});
-                } else {
-                    //Geolocation is not supported by this browser
-                }
-            }
-
-            // handle the error here
-            function positionError(error) {
-                var errorCode = error.code;
-                var message = error.message;
-
-                alert(message);
-            }
-
-            function savePosition(position) {
-                initMap(position.coords.latitude, position.coords.longitude)
-                $.post("/geolocation", {lat: position.coords.latitude, lng: position.coords.longitude, _token: "{{csrf_token()}}" });
-            }
-        </script>
-        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAg4AuvoQ6ZF5uxqpjliVxYACAdAWvbvDk&libraries=places&callback=getLocation" async defer></script>
-
+            </div>
+        </div>
     </div>
+
+    <script>
+
+        var map;
+        var infowindow;
+
+        function initMap(latitude, longitude) {
+            var pyrmont = {lat: latitude, lng: longitude};
+
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: pyrmont,
+                zoom: 15
+            });
+
+            infowindow = new google.maps.InfoWindow();
+            var service = new google.maps.places.PlacesService(map);
+            service.nearbySearch({
+                location: pyrmont,
+                radius: 500,
+                type: ['store']
+            }, callback);
+        }
+
+        function callback(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+                for (var i = 0; i < results.length; i++) {
+                    createMarker(results[i]);
+                }
+            }
+        }
+
+        function createMarker(place) {
+            var placeLoc = place.geometry.location;
+            var marker = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            });
+
+            google.maps.event.addListener(marker, 'click', function () {
+                infowindow.setContent(place.name);
+                infowindow.open(map, this);
+            });
+        }
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script>
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(savePosition, positionError, {timeout: 10000});
+            } else {
+                //Geolocation is not supported by this browser
+            }
+        }
+
+        // handle the error here
+        function positionError(error) {
+            var errorCode = error.code;
+            var message = error.message;
+
+            alert(message);
+        }
+
+        function savePosition(position) {
+            initMap(position.coords.latitude, position.coords.longitude)
+            $.post("/geolocation", {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude,
+                _token: "{{csrf_token()}}"
+            });
+        }
+    </script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAg4AuvoQ6ZF5uxqpjliVxYACAdAWvbvDk&libraries=places&callback=getLocation" async defer></script>
+
 @endsection
