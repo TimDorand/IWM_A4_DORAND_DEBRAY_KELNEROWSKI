8115edb67a636777899cbce507b07178551d01fb
SimonDebray <simon.debray@devinci.fr>
2017-10-15 01:22:30 +0200

    localsotrage pos

--- resources/assets/js/components/Maps.vue
+++ resources/assets/js/components/Maps.vue
@@ -21,31 +21,36 @@
         },
         methods: {
             getPosition() {
-                let position = localStorage.getItem('position');
-                if (position) {
-                    console.log('Stored position', position)
-                    this.getRestaurants(JSON.parse(position))
+                let lng = localStorage.getItem('lng');
+                let lat = localStorage.getItem('lat');
+                if (lat && lng) {
+                    this.initMap({coords : { latitude : lat, longitude : lng}});
                 } else {
                     navigator.geolocation.getCurrentPosition(this.initMap, this.errorHandler, {timeout: 10000});
                 }
-              /*  setInterval(function () {
+              setInterval(function () {
                     if (navigator.geolocation) {
-                        navigator.geolocation.getCurrentPosition(this.savePosition, this.errorHandler, {timeout: 10000});
+                        navigator.geolocation.getCurrentPosition(function (position){
+                            localStorage.setItem('lng', position.coords.longitude);
+                            localStorage.setItem('lat', position.coords.latitude);
+                            console.log(localStorage.getItem('lat') + ' - ' + localStorage.getItem('lng'))
+                        }, this.errorHandler, {timeout: 10000});
                     } else {
                         //Geolocation is not supported by this browser
                     }
-                }, 10000)*/
+                }, 10000)
             },
             savePosition(position){
-                localStorage.setItem('position', JSON.stringify(position));
+                localStorage.setItem('lng', position.coords.longitude);
+                localStorage.setItem('lat', position.coords.latitude);
             },
             createMarkers(places) {
                 for (let i = 0; i < places.length; i++) {
                     let marker = new google.maps.Marker({
                         map: this.map,
                         position: {lat: parseFloat(places[i].lat), lng: parseFloat(places[i].lng)}
-                    })
-                    this.markers.push(marker)
+                    });
+                    this.markers.push(marker);
                     google.maps.event.addListener(marker, 'click', function () {
                         this.infowindow.setContent(places[i].name);
                         this.infowindow.open(thismap, this);
@@ -53,12 +58,12 @@
                 }
             },
             initMap(position) {
-                console.log('Position acquiered')
-                let pyrmont = {lat: position.coords.latitude, lng: position.coords.longitude};
+                let pyrmont = {lat: parseFloat(position.coords.latitude) , lng: parseFloat(position.coords.longitude)};
+                console.log(pyrmont);
                 this.map = new google.maps.Map(document.getElementById('map'), {
                     center: pyrmont,
                     zoom: 15
-                })
+                });
                 this.infowindow = new google.maps.InfoWindow()
             },
             positionError(error) {
